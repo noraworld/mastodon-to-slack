25c7df197cb5127fb4c60373f6c27a6dab21c4ad
noraworld <mail@noraworld.com>
2018-10-19 00:28:01 +0900

    Observe user's home timeline instead of local timeline

--- src/mastodon_to_slack.rb
+++ src/mastodon_to_slack.rb
@@ -8,7 +8,7 @@ Bundler.require
 Dotenv.load
 
 MASTODON_API_VERSION = 'v1'
-MASTODON_TIMELINE    = 'public:local'
+MASTODON_TIMELINE    = 'user'
 MASTODON_ENDPOINT    = "wss://#{ENV['MASTODON_INSTANCE_HOST']}"        \
                        "/api/#{MASTODON_API_VERSION}/streaming"        \
                        "?access_token=#{ENV['MASTODON_ACCESS_TOKEN']}" \
@@ -33,7 +33,12 @@ def start_connection(request, http)
     if response.dig('event') == 'update'
       payload = JSON.parse(response.dig('payload'))
 
-      if payload.dig('account', 'acct') == ENV['MASTODON_USERNAME']
+      # Ruby style referring to https://github.com/airbnb/ruby/blob/master/README.md#newlines
+      if payload.dig('account', 'acct') == ENV['MASTODON_USERNAME']                            &&
+         (payload.dig('visibility') == 'public' || payload.dig('visibility') == 'unlisted')    &&
+         (payload.dig('mentions').empty?        || payload.dig('in_reply_to_account_id').nil?) &&
+         !payload.dig('reblogged')
+
         mastodon_status_uri = payload.dig('url')
 
         request.body = {
