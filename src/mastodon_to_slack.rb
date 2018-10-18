a71fe3d2692073d44c0d8c7be00b6fdae02edf7d
noraworld <mail@noraworld.com>
2018-10-19 02:26:59 +0900

    Add discharge mode   - post all statuses in home timeline to Slack   - this option is for sharing boosted toots

--- src/mastodon_to_slack.rb
+++ src/mastodon_to_slack.rb
@@ -3,6 +3,7 @@
 require 'bundler/setup'
 require 'net/https'
 require './lib/colorize.rb'
+require './lib/booleanize.rb'
 
 Bundler.require
 Dotenv.load
@@ -14,6 +15,7 @@ MASTODON_ENDPOINT    = "wss://#{ENV['MASTODON_INSTANCE_HOST']}"        \
                        "?access_token=#{ENV['MASTODON_ACCESS_TOKEN']}" \
                        "&stream=#{MASTODON_TIMELINE}"
 SLACK_WEBHOOK_URI    = URI.parse(ENV['SLACK_WEBHOOK_URI'])
+DISCHARGE_MODE       = ENV.fetch('DISCHARGE_MODE', 'false').booleanize
 
 request      = Net::HTTP::Post.new(SLACK_WEBHOOK_URI.request_uri)
 http         = Net::HTTP.new(SLACK_WEBHOOK_URI.host, SLACK_WEBHOOK_URI.port)
@@ -33,22 +35,22 @@ def start_connection(request, http)
     if response.dig('event') == 'update'
       payload = JSON.parse(response.dig('payload'))
 
-      # Ruby style referring to https://github.com/airbnb/ruby/blob/master/README.md#newlines
-      if payload.dig('account', 'acct') == ENV['MASTODON_USERNAME']                            &&
+      # The conditional expression below is redundant
+      # because it does same thing in `if` statement and `elsif` statement.
+      # However it is very complex to combine these statements.
+
+      # ruby style referring to https://github.com/airbnb/ruby/blob/master/README.md#newlines
+      if DISCHARGE_MODE                                                                        &&
          (payload.dig('visibility') == 'public' || payload.dig('visibility') == 'unlisted')    &&
-         (payload.dig('mentions').empty?        || payload.dig('in_reply_to_account_id').nil?) &&
-         !payload.dig('reblogged')
+         (payload.dig('mentions').empty?        || payload.dig('in_reply_to_account_id').nil?)
 
-        mastodon_status_uri = payload.dig('url')
+        post_to_slack(payload, request, http)
+      elsif payload.dig('account', 'acct') == ENV['MASTODON_USERNAME']                            &&
+            (payload.dig('visibility') == 'public' || payload.dig('visibility') == 'unlisted')    &&
+            (payload.dig('mentions').empty?        || payload.dig('in_reply_to_account_id').nil?) &&
+            !payload.dig('reblogged')
 
-        request.body = {
-          text: mastodon_status_uri,
-          unfurl_links: true
-        }.to_json
-
-        http.start do |h|
-          h.request(request)
-        end
+        post_to_slack(payload, request, http)
       end
     end
   end
@@ -65,6 +67,19 @@ def start_connection(request, http)
 
   ws.on :error do |_|
     puts 'Error occured'.red if ARGV[0] == '--verbose'
+  end
+end
+
+def post_to_slack(payload, request, http)
+  mastodon_status_uri = payload.dig('url')
+
+  request.body = {
+    text: mastodon_status_uri,
+    unfurl_links: true
+  }.to_json
+
+  http.start do |h|
+    h.request(request)
   end
 end
 
